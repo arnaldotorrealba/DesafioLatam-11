c042a3e2c7884af6249efd3085184c39bd665d1e
arnaldotorr <arnaldo.torrealba@gmail.com>
2023-08-01 11:34:21 -0500

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Variables
+let quantity = document.querySelector('#quantity');
+let color = document.querySelector('#color');
+let price = document.querySelector('#price');
+let total_p = document.querySelector('#total-p span');
+let quantity_p = document.querySelector('#quantity-p span');
+let color_c = document.querySelector('#color-circle');
+
+let productPrice = 149990;
+
+/// Dándole formato al precio
+price.innerHTML = new Intl.NumberFormat("de-DE", { style: "currency", currency: "CLP" }).format(productPrice);
+
+function total(){                   
+    let total = Number(quantity.value) * productPrice;
+    total_p.innerHTML = new Intl.NumberFormat("de-DE", { style: "currency", currency: "CLP" }).format(total);
+    quantity_p.innerHTML = quantity.value;
+    color_c.style.backgroundColor = color.value;
+}
